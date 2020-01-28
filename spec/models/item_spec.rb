69caf8c15b54f2cef53f93f498547165bddc5838
Alison Vermeil <alison.minton@gmail.com>
2020-01-27 20:00:26 -0700

    Add merchant_id foreign key to items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Item, type: :model do
     it {should validate_presence_of :description}
     it {should validate_presence_of :unit_price}
   end
+
+  describe "relationships" do
+    it {should belong_to :merchant}
+  end
 end
