bde6d443c93fc0cb8e202f8e142f700ca6bdcca1
Alison Vermeil <alison.minton@gmail.com>
2020-01-27 20:15:11 -0700

    Add customer_id to invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_031228) do
+ActiveRecord::Schema.define(version: 2020_01_28_031421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2020_01_28_031228) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id"
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_invoices_on_customer_id"
     t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
 
@@ -46,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_01_28_031228) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoices", "customers"
   add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
 end
