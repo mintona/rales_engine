d820c392115292c0601bcc8f2101618bdee17e70
Alison Vermeil <alison.minton@gmail.com>
2020-01-27 20:28:57 -0700

    Finalize relationships in models for invoice item many to many relationship

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,6 @@ class Invoice < ApplicationRecord
   validates_presence_of :status
   belongs_to :merchant
   belongs_to :customer
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,6 @@ class Item < ApplicationRecord
   validates_numericality_of :unit_price
 
   belongs_to :merchant
+  has_many :invoice_items
+  has_many :invoices, through: :invoice_items
 end
