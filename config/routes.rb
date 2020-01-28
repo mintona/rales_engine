b3e4db2c1cf6f292b57ba6c694404e63953f4383
Alison Vermeil <alison.minton@gmail.com>
2020-01-28 15:40:53 -0700

    Add find route and controller action, to be refactored

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'merchants/find', to: 'merchants#find'
       resources :merchants, only: [:index, :show]
+
     end
   end
 end
