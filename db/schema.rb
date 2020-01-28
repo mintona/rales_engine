22f84d7ebca88a62402f38edb2e033ed4755f871
Alison Vermeil <alison.minton@gmail.com>
2020-01-27 20:13:20 -0700

    Add merchant_id to invoices

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_030735) do
+ActiveRecord::Schema.define(version: 2020_01_28_031228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_01_28_030735) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -44,5 +46,6 @@ ActiveRecord::Schema.define(version: 2020_01_28_030735) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
 end
