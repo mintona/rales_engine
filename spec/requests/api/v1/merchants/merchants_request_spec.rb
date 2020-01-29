22692f47d23c669523e6c2ae782203d93ca90237
Alison Vermeil <alison.minton@gmail.com>
2020-01-29 16:22:07 -0700

    Restructure test files

--- spec/requests/api/v1/merchants/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants/merchants_request_spec.rb
@@ -1,64 +1,66 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "sends a list of merchants" do
-    create_list(:merchant, 3)
+  describe "record endpoints" do
+    it "sends a list of merchants" do
+      create_list(:merchant, 3)
 
-    get '/api/v1/merchants'
+      get '/api/v1/merchants'
 
-    expect(response).to be_successful
-
-    merchants = JSON.parse(response.body)['data']
-
-    expect(merchants.count).to eq(3)
-    expect(merchants.first.keys).to eq(["id", "type", "attributes", "relationships"])
-  end
-
-  it "can get one merchant by its id" do
-    id = create(:merchant).id.to_s
-
-    get "/api/v1/merchants/#{id}"
-
-    merchant = JSON.parse(response.body)['data']
-
-    expect(response).to be_successful
-    expect(merchant["id"]).to eq(id)
-  end
-
-  describe "can get one merchant by any attribute:" do
-    before :each do
-      @merchant = create(:merchant)
-    end
-
-    it "find by id" do
-      get "/api/v1/merchants/find?id=#{@merchant.id}"
-
-      merchant = JSON.parse(response.body)['data']
       expect(response).to be_successful
 
-      expect(merchant['attributes']['id']).to eq(@merchant.id)
+      merchants = JSON.parse(response.body)['data']
+
+      expect(merchants.count).to eq(3)
+      expect(merchants.first.keys).to eq(["id", "type", "attributes", "relationships"])
     end
 
-    it "name" do
-      get "/api/v1/merchants/find?name=#{@merchant.name}"
+    it "can get one merchant by its id" do
+      id = create(:merchant).id.to_s
+
+      get "/api/v1/merchants/#{id}"
 
       merchant = JSON.parse(response.body)['data']
 
       expect(response).to be_successful
-      expect(merchant['attributes']['name']).to eq(@merchant.name)
+      expect(merchant["id"]).to eq(id)
     end
+  describe "single finders" do
+    describe "can get one merchant by any attribute:" do
+      before :each do
+        @merchant = create(:merchant)
+      end
 
-    xit "created_at" do
-      get "/api/v1/merchants/find?created_at=#{@merchant.created_at}"
-      merchant = JSON.parse(response.body)['data']
+      it "find by id" do
+        get "/api/v1/merchants/find?id=#{@merchant.id}"
 
-      expect(response).to be_successful
-      expect(merchant['attributes']['name']).to eq(@merchant.name)
-    end
+        merchant = JSON.parse(response.body)['data']
+        expect(response).to be_successful
 
-    xit "updated_at" do
-      get "/api/v1/merchants/find?updated_at=#{@merchant.updated_at}"
+        expect(merchant['attributes']['id']).to eq(@merchant.id)
+      end
 
+      it "name" do
+        get "/api/v1/merchants/find?name=#{@merchant.name}"
+
+        merchant = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+        expect(merchant['attributes']['name']).to eq(@merchant.name)
+      end
+
+      xit "created_at" do
+        get "/api/v1/merchants/find?created_at=#{@merchant.created_at}"
+        merchant = JSON.parse(response.body)['data']
+
+        expect(response).to be_successful
+        expect(merchant['attributes']['name']).to eq(@merchant.name)
+      end
+
+      xit "updated_at" do
+        get "/api/v1/merchants/find?updated_at=#{@merchant.updated_at}"
+
+      end
     end
   end
 
@@ -136,6 +138,15 @@ describe "Merchants API" do
       # ORDER BY revenue DESC
       # LIMIT 1;
 
+      end
     end
   end
 end
+#
+# rales_engine_development=# SELECT merchants.*, sum(invoice_items.quantity) AS total_sold
+# rales_engine_development-# FROM merchants
+#
+# rales_engine_development-# INNER JOIN items ON items.merchant_id = merchants.id
+# rales_engine_development-# INNER JOIN invoice_items ON invoice_items.item_id = items.id
+# rales_engine_development-# INNER JOIN transactions ON transactions.invoice_id = invoice_items.invoice_id
+# maybe we dont need this one now? rales_engine_development-# INNER JOIN invoices ON invoice_items.invoice_id = invoices.id
