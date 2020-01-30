53ccc72f3ff29d96f6e178c8e17e0aa6fdf55b19
Alison Vermeil <alison.minton@gmail.com>
2020-01-29 18:43:16 -0700

    Add class method for merchant highest_revenue, wip

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,10 @@ class Merchant < ApplicationRecord
 
   has_many :items
   has_many :invoices
+  #invoice_items, through: :invoices
+  #transactions, through: :invoices
+
+  def self.highest_revenue(limit)
+    select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS revenue").joins("INNER JOIN invoices ON invoices.merchant_id = merchants.id INNER JOIN invoice_items ON invoice_items.invoice_id = invoices.id INNER JOIN transactions ON transactions.invoice_id = invoices.id").where(transactions: {result: "success"}).group(:id).order("revenue DESC").limit(limit)
+  end
 end
