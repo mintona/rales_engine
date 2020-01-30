5cbbf19b24d2a1038ed6115d1c2be23f65b5350d
Alison Vermeil <alison.minton@gmail.com>
2020-01-29 19:19:41 -0700

    Refactor highest_revenue class method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,7 +7,6 @@ class Merchant < ApplicationRecord
   #transactions, through: :invoices
 
   def self.highest_revenue(limit)
-    # joins(invoices: [:invoice_items, :transactions]).select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS revenue").group(:id).where(transactions: {result: "success"}).order("revenue DESC").limit(limit)
     joins(invoices: [:invoice_items, :transactions]).select("merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) AS revenue").group(:id).merge(Transaction.successful).order("revenue DESC").limit(limit)
   end
 end
