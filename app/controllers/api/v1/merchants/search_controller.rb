c511d036fcacf05e0247ca8bc44e0ded91572d43
Alison Vermeil <alison.minton@gmail.com>
2020-01-29 20:13:36 -0700

    Refactor merchants find action to work from a new merchants search controller

---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::Merchants::SearchController < ApplicationController
+  def show
+    render json: MerchantSerializer.new(Merchant.find_by(search_params))
+  end
+
+  private
+    def search_params
+      params.permit(:id, :name, :created_at, :updated_at)
+    end
+end
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -6,21 +6,4 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
-
-#come back to this to finish created_at and updated_at
-  def find
-    if params[:id]
-      render json: MerchantSerializer.new(Merchant.find_by(id: params[:id]))
-    elsif params[:name]
-      render json: MerchantSerializer.new(Merchant.find_by(name: params[:name]))
-    # elsif params[:created_at]
-    #   render json: MerchantSerializer.new(Merchant.find_by(created_at: params[:created_at].in_time_zone))
-    end
-  end
-
-  # private
-  #request.query_params
-  #   def find_params
-  #     params.permit(:id, :name, :updated_at, :created_at)
-  #   end
 end
