466bd2d88cbad406d859c239dddb72c24474a020
Alison Vermeil <alison.minton@gmail.com>
2020-01-30 15:20:29 -0700

    Make case insensitive searching scope methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,11 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  scope :find_all_case_insensitive, lambda { |attribute, value| where("LOWER(#{attribute}) = ?", value.downcase)}
+  scope :find_one_case_insensitive, lambda { |attribute, value| find_by("LOWER(#{attribute}) = ?", value.downcase)}
+  # scope :find_all_case_insensitive, -> (attribute, value) { where("LOWER(#{attribute}) = ?", value.downcase)}
+  # scope :find_all_by_name_case_insensitive, -> (name) { where("LOWER(name) = ?", name.downcase)}
+  scope :find_by_name_case_insensitive, -> (name) { find_by("LOWER(name) = ?", name.downcase)}
+
 end
