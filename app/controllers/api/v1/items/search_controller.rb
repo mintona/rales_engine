fb017ad3db772e8f9e62600070af9b473b110b6f
Alison Vermeil <alison.minton@gmail.com>
2020-01-30 15:56:10 -0700

    Refactor show action to also use case insensitive searching for description

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Items::SearchController < ApplicationController
   def show
-    if params[:name]
+    if params[:name] || params[:description]
       render json: ItemSerializer.new(Item.find_one_case_insensitive(search_params))
     else
       render json: ItemSerializer.new(Item.find_by(search_params))
