06c0fcd99dcaeb027fc1efcd1842eb28a92ca54c
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 16:54:02 -0700

    Add favorite_merchant method and customer scope to invoice and

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,4 +6,6 @@ class Invoice < ApplicationRecord
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :transactions
+
+  scope :customer, -> (id) {where("customer_id = ?", id)}
 end
