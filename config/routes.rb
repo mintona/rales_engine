7f1f0ed59ed96ea22d63e51e39307e026ca851c9
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 11:33:04 -0700

    Add route for items/:id/merchant

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
       resources :items, only: [:index, :show] do
         scope module: 'items' do
           resources :invoice_items, only: [:index]
+          get '/merchant', to: 'merchants#show'
         end
       end
     end
