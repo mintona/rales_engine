6fb0db3a49a4ae9391c788513684567bbdf47a78
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 16:07:18 -0700

    Add test for item best day

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -333,8 +333,6 @@ describe "Items API" do
 
   describe "business logic" do
     it "returns the top 'x' items ranked by total revenue" do
-      # GET /api/v1/items/most_revenue?quantity=x returns the top x items ranked by total revenue generated
-      # create_list(:merchant, 5)
       create(:item, unit_price: 300)
       create(:item, unit_price: 400)
       item_3 = create(:item, unit_price: 100)
@@ -351,10 +349,7 @@ describe "Items API" do
         else
           create(:transaction, result: "success", invoice: invoice)
         end
-        # items = merchant.items
-        # items.each do |item|
-          item.invoice_items.create!(quantity: 10, unit_price: item.unit_price, invoice: invoice)
-        # end
+        item.invoice_items.create!(quantity: 10, unit_price: item.unit_price, invoice: invoice)
       end
 
       x = 1
@@ -376,14 +371,34 @@ describe "Items API" do
       end
     end
 
-    xit "returns the total revenue for date 'x' across all merchants" do
-      # GET /api/v1/items/:id/best_day returns the date with the most sales for the given item using the invoice date. If there are multiple days with equal number of sales, return the most recent day.
-      x = "2012-03-16"
-      y = "2012-03-07"
+    it "returns the date with the most sales for the given item using the invoice date" do
+      #If there are multiple days with equal number of sales, return the most recent day.
+      item = create(:item, unit_price: 100)
+      invoice_1 = create(:invoice, created_at: "1985-02-04")
+      invoice_2 = create(:invoice, created_at: "2020-02-01")
+      invoice_3 = create(:invoice, created_at: "2020-01-30")
+      invoice_4 = create(:invoice, created_at: "2018-02-04")
+      invoice_5 = create(:invoice, created_at: "2021-02-04")
 
-      get "/api/v1/merchants/revenue?date=#{x}"
+      create(:transaction, invoice: invoice_1)
+      create(:transaction, invoice: invoice_2)
+      create(:transaction, invoice: invoice_3)
+      create(:transaction, invoice: invoice_4)
+      create(:transaction, result: 'failed', invoice: invoice_5)
 
+      item.invoice_items.create!(quantity: 10, unit_price: item.unit_price, invoice: invoice_1)
+      item.invoice_items.create!(quantity: 1, unit_price: item.unit_price, invoice: invoice_2)
+      item.invoice_items.create!(quantity: 5, unit_price: item.unit_price, invoice: invoice_3)
+      item.invoice_items.create!(quantity: 10, unit_price: item.unit_price, invoice: invoice_4)
+      item.invoice_items.create!(quantity: 100, unit_price: item.unit_price, invoice: invoice_5)
 
+      get "/api/v1/items/#{item.id}/best_day"
+
+      expect(response).to be_successful
+
+      best_day = JSON.parse(response.body)['data']
+      
+      expect(best_day['attributes']['best_day']).to eq("2018-02-04")
     end
    end
 end
