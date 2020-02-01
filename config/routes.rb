eb9e76de78337386af5a526c38ff48b47b52f892
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 10:25:13 -0700

    Add route for items/random

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       end
 
       namespace :items do
+        get '/random', to: 'random#show'
         get 'find', to: 'search#show'
         get 'find_all', to: 'search#index'
       end
