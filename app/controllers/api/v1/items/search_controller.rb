a4d8aac420934844ca99223c5d72f6d985b1f0e3
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 09:44:45 -0700

    Add merchant_id to permitted params

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::Items::SearchController < ApplicationController
   private
     def search_params
       convert_unit_price if params[:unit_price]
-      params.permit(:id, :name, :description, :unit_price, :created_at, :updated_at)
+      params.permit(:id, :name, :description, :merchant_id, :unit_price, :created_at, :updated_at)
     end
 
     def convert_unit_price
