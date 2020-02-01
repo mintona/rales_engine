78e122db0aeb42d24f95363dbf5bf2a3fd0a942b
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 12:22:35 -0700

    Remove comments

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,4 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
-
-  # scope :find_all_case_insensitive, lambda { |attribute, value| where("LOWER(#{attribute}) = ?", value.downcase)}
-  # scope :find_one_case_insensitive, lambda { |attribute, value| find_by("LOWER(#{attribute}) = ?", value.downcase)}
-  # scope :find_all_case_insensitive, -> (attribute, value) { where("LOWER(#{attribute}) = ?", value.downcase)}
-  # scope :find_all_by_name_case_insensitive, -> (name) { where("LOWER(name) = ?", name.downcase)}
-  # scope :find_by_name_case_insensitive, -> (name) { find_by("LOWER(name) = ?", name.downcase)}
-
 end
