bbea018ee06f975b691375356dfd5016b66f8cf8
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 09:00:04 -0700

    Add find all case insensitive method to application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,6 +4,12 @@ class ApplicationRecord < ActiveRecord::Base
   def self.find_one_case_insensitive(search_params = {})
     attribute = search_params.keys.first
     value = search_params[attribute]
-    self.where("LOWER(#{attribute}) = ?", value.downcase).first
+    self.find_by("LOWER(#{attribute}) = ?", value.downcase)
+  end
+
+  def self.find_all_case_insensitive(search_params = {})
+    attribute = search_params.keys.first
+    value = search_params[attribute]
+    self.where("LOWER(#{attribute}) = ?", value.downcase)
   end
 end
