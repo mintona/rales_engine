fd6943d91acc293d30ec9dd0aedb3b1501a9b0a9
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 16:08:20 -0700

    Add show action to revenue controller

--- app/controllers/api/v1/items/revenue_controller.rb
+++ app/controllers/api/v1/items/revenue_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::Items::RevenueController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.highest_revenue(params[:quantity]))
   end
+
+  def show
+    item = Item.find(params[:item_id])
+    render json: { data: {attributes: {best_day: item.best_day}}}
+    # render json: ItemSerializer.new(Item.find(params[:item_id]))
+  end
 end
