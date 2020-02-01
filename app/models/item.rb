3567001eb298dfe99883620e9d2504a777e1881b
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 16:11:14 -0700

    Add test and model method for item best_day

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def best_day
+    Invoice.joins(:invoice_items, :transactions).select("invoices.created_at, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue").where("invoice_items.item_id = #{self.id}").group(:id).merge(Transaction.successful).order("revenue DESC, invoices.created_at DESC").limit(1).first.created_at.strftime('%Y-%m-%d')
+  end
 end
