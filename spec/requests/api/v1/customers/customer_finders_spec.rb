fb7a54970e27198ed019f36e4b9d3f226e8f97a2
Alison Vermeil <alison.minton@gmail.com>
2020-02-02 14:01:48 -0700

    Add all tests for remaining customer finders

--- spec/requests/api/v1/customers/customer_finders_spec.rb
+++ spec/requests/api/v1/customers/customer_finders_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Customers API" do
   describe "finders" do
     describe "single finders" do
-      describe "can get one item by any attribute:" do
+      describe "can get one customer by any attribute:" do
         before :each do
           @customer = create(:customer, created_at: "2020-01-30", updated_at: "2020-01-31")
           create_list(:customer, 2)
@@ -46,7 +46,7 @@ RSpec.describe "Customers API" do
           end
         end
 
-        xit "created_at" do
+        it "created_at" do
           date = @customer.created_at.to_s
 
           get "/api/v1/customers/find?created_at=#{date}"
@@ -57,7 +57,7 @@ RSpec.describe "Customers API" do
           expect(customer['attributes']['id']).to eq(@customer.id)
         end
 
-        xit "updated_at" do
+        it "updated_at" do
           date = @customer.updated_at.to_s
 
           get "/api/v1/customers/find?updated_at=#{date}"
@@ -72,14 +72,14 @@ RSpec.describe "Customers API" do
     describe "multi-finders" do
       describe "return all matches by any attribute" do
         before :each do
-          @item_1 = create(:item, created_at: "19-12-05", updated_at: "20-02-04")
-          @item_2 = create(:item, created_at: "19-12-25", updated_at: "20-03-05")
-          @item_3 = create(:item, merchant: @item_2.merchant, created_at: "19-12-25", updated_at: "20-02-04")
-          @item_4 = create(:item, name: @item_3.name, description: @item_3.description, created_at: "20-1-30", updated_at: "20-03-05")
+          @customer_1 = create(:customer, created_at: "19-12-05", updated_at: "20-02-04")
+          @customer_2 = create(:customer, created_at: "19-12-25", updated_at: "20-03-05")
+          @customer_3 = create(:customer, created_at: "19-12-25", updated_at: "20-02-04")
+          @customer_4 = create(:customer, first_name: @customer_3.first_name, last_name: @customer_3.last_name, created_at: "20-1-30", updated_at: "20-03-05")
         end
 
-        xit "find all by id" do
-          get "/api/v1/customers/find_all?id=#{@item_1.id}"
+        it "find all by id" do
+          get "/api/v1/customers/find_all?id=#{@customer_1.id}"
 
           expect(response).to be_successful
 
@@ -87,78 +87,57 @@ RSpec.describe "Customers API" do
 
           expect(customers.count).to eq(1)
 
-          expect(customers.first['attributes']['id']).to eq(@item_1.id)
+          expect(customers.first['attributes']['id']).to eq(@customer_1.id)
         end
 
-        xit "find all by merchant_id" do
-          get "/api/v1/customers/find_all?merchant_id=#{@item_1.merchant_id}"
+        it "find all by first_name" do
+          customer_1_first_names = [@customer_1.first_name, @customer_1.first_name.upcase, @customer_1.first_name.downcase]
 
-          expect(response).to be_successful
-
-          customers = JSON.parse(response.body)['data']
-
-          expect(customers.count).to eq(1)
-
-          expect(customers.first['attributes']['id']).to eq(@item_1.id)
-
-          get "/api/v1/customers/find_all?merchant_id=#{@item_2.merchant_id}"
-
-          customers = JSON.parse(response.body)['data']
-
-          expect(customers.count).to eq(2)
-
-          expect(customers.first['attributes']['id']).to eq(@item_2.id)
-          expect(customers.last['attributes']['id']).to eq(@item_3.id)
-        end
-
-        xit "find all by name" do
-          item_1_names = [@item_1.name, @item_1.name.upcase, @item_1.name.downcase]
-
-          item_1_names.each do |name|
-            get "/api/v1/customers/find_all?name=#{name}"
+          customer_1_first_names.each do |first_name|
+            get "/api/v1/customers/find_all?first_name=#{first_name}"
             expect(response).to be_successful
             customers = JSON.parse(response.body)['data']
             expect(customers.count).to eq(1)
-            expect(customers.first['attributes']['id']).to eq(@item_1.id)
+            expect(customers.first['attributes']['id']).to eq(@customer_1.id)
           end
 
-          name_2 = @item_3.name
+          name_2 = @customer_3.first_name
 
-          get "/api/v1/customers/find_all?name=#{name_2}"
+          get "/api/v1/customers/find_all?first_name=#{name_2}"
 
           customers = JSON.parse(response.body)['data']
 
           expect(customers.count).to eq(2)
 
-          expect(customers.first['attributes']['id']).to eq(@item_3.id)
-          expect(customers.last['attributes']['id']).to eq(@item_4.id)
+          expect(customers.first['attributes']['id']).to eq(@customer_3.id)
+          expect(customers.last['attributes']['id']).to eq(@customer_4.id)
         end
 
-        xit "find all by description" do
-          item_1_descriptions = [@item_1.description, @item_1.description.upcase, @item_1.description.downcase]
+        it "find all by last_name" do
+          customer_1_last_names = [@customer_1.last_name, @customer_1.last_name.upcase, @customer_1.last_name.downcase]
 
-          item_1_descriptions.each do |description|
-            get "/api/v1/customers/find_all?description=#{description}"
+          customer_1_last_names.each do |last_name|
+            get "/api/v1/customers/find_all?last_name=#{last_name}"
             expect(response).to be_successful
             customers = JSON.parse(response.body)['data']
             expect(customers.count).to eq(1)
-            expect(customers.first['attributes']['id']).to eq(@item_1.id)
+            expect(customers.first['attributes']['id']).to eq(@customer_1.id)
           end
 
-          description_2 = @item_3.description
+          last_name_2 = @customer_3.last_name
 
-          get "/api/v1/customers/find_all?description=#{description_2}"
+          get "/api/v1/customers/find_all?last_name=#{last_name_2}"
 
           customers = JSON.parse(response.body)['data']
 
           expect(customers.count).to eq(2)
 
-          expect(customers.first['attributes']['id']).to eq(@item_3.id)
-          expect(customers.last['attributes']['id']).to eq(@item_4.id)
+          expect(customers.first['attributes']['id']).to eq(@customer_3.id)
+          expect(customers.last['attributes']['id']).to eq(@customer_4.id)
         end
 
-        xit "find all by created_at" do
-          date_1 = @item_1.created_at
+        it "find all by created_at" do
+          date_1 = @customer_1.created_at
 
           get "/api/v1/customers/find_all?created_at=#{date_1}"
 
@@ -168,9 +147,9 @@ RSpec.describe "Customers API" do
 
           expect(customers.count).to eq(1)
 
-          expect(customers.first['attributes']['id']).to eq(@item_1.id)
+          expect(customers.first['attributes']['id']).to eq(@customer_1.id)
 
-          date_2 = @item_2.created_at
+          date_2 = @customer_2.created_at
 
           get "/api/v1/customers/find_all?created_at=#{date_2}"
 
@@ -178,12 +157,12 @@ RSpec.describe "Customers API" do
 
           expect(customers.count).to eq(2)
 
-          expect(customers.first['attributes']['id']).to eq(@item_2.id)
-          expect(customers.last['attributes']['id']).to eq(@item_3.id)
+          expect(customers.first['attributes']['id']).to eq(@customer_2.id)
+          expect(customers.last['attributes']['id']).to eq(@customer_3.id)
         end
 
-        xit "find all by updated_at" do
-          date_1 = @item_1.updated_at
+        it "find all by updated_at" do
+          date_1 = @customer_1.updated_at
 
           get "/api/v1/customers/find_all?updated_at=#{date_1}"
 
@@ -193,10 +172,10 @@ RSpec.describe "Customers API" do
 
           expect(customers.count).to eq(2)
 
-          expect(customers.first['attributes']['id']).to eq(@item_1.id)
-          expect(customers.last['attributes']['id']).to eq(@item_3.id)
+          expect(customers.first['attributes']['id']).to eq(@customer_1.id)
+          expect(customers.last['attributes']['id']).to eq(@customer_3.id)
 
-          date_2 = @item_2.updated_at
+          date_2 = @customer_2.updated_at
 
           get "/api/v1/customers/find_all?updated_at=#{date_2}"
 
@@ -204,30 +183,30 @@ RSpec.describe "Customers API" do
 
           expect(customers.count).to eq(2)
 
-          expect(customers.first['attributes']['id']).to eq(@item_2.id)
-          expect(customers.last['attributes']['id']).to eq(@item_4.id)
+          expect(customers.first['attributes']['id']).to eq(@customer_2.id)
+          expect(customers.last['attributes']['id']).to eq(@customer_4.id)
         end
       end
     end
 
     describe "random" do
-      xit "returns a random item" do
-        customers = create_list(:item, 10)
+      it "returns a random customer" do
+        customers = create_list(:customer, 10)
 
         get "/api/v1/customers/random"
 
         expect(response).to be_successful
 
-        number_of_items = JSON.parse(response.body).count
-        expect(number_of_items).to eq(1)
+        number_of_customers = JSON.parse(response.body).count
+        expect(number_of_customers).to eq(1)
 
-        random_item = JSON.parse(response.body)['data']
+        random_customer = JSON.parse(response.body)['data']
 
-        expect(random_item['type']).to eq('item')
-        expect(random_item['attributes'].keys).to match_array(['id', 'name', 'description', 'unit_price', 'merchant_id'])
+        expect(random_customer['type']).to eq('customer')
+        expect(random_customer['attributes'].keys).to match_array(['id', 'first_name', 'last_name'])
 
         # could test the range instead?
-        result = customers.one? { |item| item.id == random_item['attributes']['id'] }
+        result = customers.one? { |customer| customer.id == random_customer['attributes']['id'] }
         expect(result).to be(true)
       end
       end
