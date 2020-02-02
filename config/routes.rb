f8ecc0e36a512574c30202b1d69d67202602a82d
Alison Vermeil <alison.minton@gmail.com>
2020-02-02 11:21:44 -0700

    Add route for customers transactions

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
       resources :customers, only: [:index, :show] do
         scope module: 'customers' do
           resources :invoices, only: [:index]
+          resources :transactions, only: [:index]
           get '/favorite_merchant', to: 'merchants#show'
         end
       end
