3a7aaa822537147e115e87d792bef1d6c754a8b5
Alison Vermeil <alison.minton@gmail.com>
2020-02-01 19:39:35 -0700

    Hard-code json return to be refactored, passes spec harness

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -5,12 +5,6 @@ class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
     date = params[:date]
-      # invoices = Invoice.joins(:invoice_items, :transactions).select("invoices.created_at, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue").where("DATE(invoices.created_at) = '#{date}'").group("invoices.created_at").merge(Transaction.successful).order("revenue desc")
-      # revenues = invoices.map { |invoice| invoice.revenue }
-      # daily_total = revenues.sum
-      # "#{daily_total/100.to_f}"
-require "pry"; binding.pry
-    render json: MerchantRevenueSerializer.new(Merchant.total_revenue_by_date(date))
-    # render json: { data: { attributes: { total_revenue: Merchant.total_revenue_by_date(date)}}}
+    render json: { data: { attributes: { total_revenue: Merchant.total_revenue_by_date(date)}}}
   end
 end
