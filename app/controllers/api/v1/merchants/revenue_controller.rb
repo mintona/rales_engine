d68bf591476463149a9ad6c3c142e693b138df5d
Alison Vermeil <alison.minton@gmail.com>
2020-02-02 09:01:00 -0700

    Remove merchant revenue serializer for now, wip

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -5,6 +5,8 @@ class Api::V1::Merchants::RevenueController < ApplicationController
 
   def show
     date = params[:date]
+    # total_revenue = Merchant.total_revenue_by_date(date)
+    # render json: MerchantRevenueSerializer.new(Merchant.total_revenue(date))
     render json: { data: { attributes: { total_revenue: Merchant.total_revenue_by_date(date)}}}
   end
 end
--- app/serializers/merchant_revenue_serializer.rb
+++ app/serializers/merchant_revenue_serializer.rb
@@ -1,4 +1,8 @@
-class MerchantRevenueSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :total_revenue_by_date
-end
+# class MerchantRevenueSerializer
+#   include FastJsonapi::ObjectSerializer
+#
+#   # alias_attribute :total_revenue_by_date, :total_revenue
+#   # attribute :total_revenue do |daily_total|
+#   #   "#{daily_total/100.to_f}"
+#   # end
+# end
